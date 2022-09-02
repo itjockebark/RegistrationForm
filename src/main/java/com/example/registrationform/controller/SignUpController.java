829e5d4935da2d5d46a8a8a1483344c92363bb45
itjockebark <jockebark@msn.com>
2022-09-02 17:40:16 +0200

    Adds body background
    Implements 2 new images
    
    Adjusts color pattern
    
    Fixes the signup-form page

--- src/main/java/com/example/registrationform/controller/SignUpController.java
+++ src/main/java/com/example/registrationform/controller/SignUpController.java
@@ -3,10 +3,16 @@ package com.example.registrationform.controller;
 import com.example.registrationform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SignUpController {
 
     @Autowired
     UserService userService;
+
+    @GetMapping("/signup-form")
+    public String signupForm() {
+        return "signup-form";
+    }
 }
