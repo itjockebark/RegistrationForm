f28c91ea749838525ebe1f4572eef1d406f081bd
itjockebark <jockebark@msn.com>
2022-09-02 14:17:22 +0200

    Implements the layout for the login page

--- src/main/java/com/example/registrationform/model/User.java
+++ src/main/java/com/example/registrationform/model/User.java
@@ -11,15 +11,13 @@ public class User {
     private Integer id;
     String username;
     String password;
-    String email;
 
     public User() {
     }
 
-    public User(String username, String password, String email) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.email = email;
     }
 
     public void setId(Integer id) {
@@ -44,13 +42,5 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
